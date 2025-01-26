429d3535f445f90fa54651bc53db891d54fd94e8
Barry Deen <barrydeen@protonmail.com>
2025-01-26 11:14:58 -0500

    add qr code, add sat amount, center everything

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import express, { Response } from 'express';
-import { config } from 'dotenv'
+import express, { Response } from "express";
+import { config } from "dotenv";
 config();
 
 const app = express();
@@ -7,42 +7,46 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
-const sseClients: Response[] = []
+const sseClients: Response[] = [];
 
-app.use(express.static('./assets/images')); 
-app.set('view engine', 'ejs');
-app.set('views', './views'); // Folder for EJS templates
-app.get('/', (req, res) => {
-  res.render('spinner', { 
+app.use(express.static("./assets/images"));
+app.set("view engine", "ejs");
+app.set("views", "./views"); // Folder for EJS templates
+
+app.get("/", (req, res) => {
+  res.render("spinner", {
     lnAddress: process.env.LIGHTNING_ADDRESS,
   });
 });
 
-app.get('/events', (req, res) => {
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection', 'keep-alive');
+app.get("/events", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
 
-    sseClients.push(res)
-    
-    const keepAlive = setInterval(() => {
-        res.write(': keep-alive\n\n');
-    }, 30000);
+  sseClients.push(res);
 
-    req.on('close', () => {
-        clearInterval(keepAlive);
-    });
+  const keepAlive = setInterval(() => {
+    res.write(": keep-alive\n\n");
+  }, 30000);
+
+  req.on("close", () => {
+    clearInterval(keepAlive);
+  });
 });
 
-app.post('/tip', (req, res) => {
-    sseClients.forEach((client: { write: (arg0: string) => void; }) => {
-        client.write(`data: new tip recorded\n\n`);
-    });
-    res.status(200).send("Tip recorded successfully");
+app.post("/tip", (req, res) => {
+  const { amount_sats } = req.body.data;
+
+  sseClients.forEach((client: Response) => {
+    client.write(`data: ${JSON.stringify({ amount_sats })}\n\n`);
+  });
+
+  res.status(200).send("Tip recorded successfully");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 export default app;
